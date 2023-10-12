e75a388508a3a60a7754e262b1b568a9de148dc7
Daniel Casado <jdanielca@gmail.com>
2023-10-12 13:58:06 -0600

    Add signature and links.

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -6,41 +6,85 @@ import sidebarStyles from "./sidebar.module.css";
 import { useAppSelector } from "@/app/model/hooks";
 import { getRepoCards } from "@/app/model/reposSlice";
 import SearchInput from "../autocomplete/input";
+import Image from "next/image";
 
 const Sidebar = () => {
   const repos = useAppSelector(getRepoCards);
 
   return (
     <div
-      style={sidebarStyles}
       className={`
-      ${sidebarStyles.sidebar}
-      w-2/5
-      h-screen
-      pt-[71px]
-      pb-[43px]
-      px-6
-      flex
-      flex-col
-      gap-4
-    `}
+        ${sidebarStyles.sidebar}
+        h-screen
+        flex
+        flex-col
+        w-2/5
+        pt-[71px]
+        pb-[43px]
+        px-6
+        gap-4
+      `}
     >
-      <SearchInput />
-      {/* Card container */}
       <div
-        className="
+        className={`
+        h-full
         flex
         flex-col
         gap-4
-      "
+      `}
       >
-        {repos.length ? (
-          repos.map((repo) => {
-            return <RepoCard key={repo.id} {...repo} />;
-          })
-        ) : (
-          <EmptyState />
-        )}
+        <SearchInput />
+        {/* Card container */}
+        <div
+          className="
+        flex
+        flex-col
+        gap-4
+        overflow-y-scroll
+      "
+        >
+          {repos.length ? (
+            repos.map((repo) => {
+              return <RepoCard key={repo.id} {...repo} />;
+            })
+          ) : (
+            <EmptyState />
+          )}
+        </div>
+      </div>
+      <div
+        className="
+          text-white
+          flex
+          align-middle
+          justify-end
+          gap-2
+        "
+      >
+        <span>
+          Made by&nbsp;
+          <a
+            href="https://d1000100.dev"
+            className="
+              text-blue-400
+              hover:underline
+            "
+          >
+            Daniel Casado (D1000100)
+          </a>
+        </span>
+        <a href="https://github.com/d01000100">
+          <Image
+            width={12}
+            height={12}
+            style={{
+              aspectRatio: 1,
+              width: "24px",
+            }}
+            src="/github-mark-white.svg"
+            alt="Link to my github"
+          />
+        </a>
       </div>
     </div>
   );
